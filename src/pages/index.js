8479bfe0a6db54ab85201dd842148309c48517a5
Özgün Özdemir <ozgunozdemir92@gmail.com>
2020-05-04 22:11:30 +0300

    added photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import './index.css';
 
-const IndexPage = () => (
-  <div>
-    <h1>Merhaba</h1>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div className="container">
+      <div className="photos">
+        {
+          data.allFile.edges.map(({ node }) => {
+            return <Img key={node.id} fluid={node.childImageSharp.fluid}></Img>
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+  allFile(filter: {absolutePath: {regex: "//photos//"}}){
+    edges{
+      node{
+        id
+        childImageSharp{
+          fluid{
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
